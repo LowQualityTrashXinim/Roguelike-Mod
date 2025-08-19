eed6bb49c3765a769d183534573a2ea9f3a27044
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2025-08-19 16:02:03 +0700

    extremely very minor change

--- Contents/Items/RelicItem/RelicSetContent/RelicSet_TitanicBlood.cs
+++ Contents/Items/RelicItem/RelicSetContent/RelicSet_TitanicBlood.cs
@@ -1,12 +1,6 @@
-﻿using Roguelike.Common.Utils;
-using Roguelike.Contents.Items.Accessories.LostAccessories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria;
+﻿using Terraria;
 using Terraria.ModLoader;
+using Roguelike.Common.Utils;
 
 namespace Roguelike.Contents.Items.RelicItem.RelicSetContent;
 internal class TitanicBlood_ModPlayer : ModPlayer {
