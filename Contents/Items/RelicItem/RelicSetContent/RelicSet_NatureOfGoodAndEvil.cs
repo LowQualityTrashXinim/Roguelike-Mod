913680f7820ad151b96987005d7bd2b37f7047da
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2025-08-19 16:23:28 +0700

    Check the changelog for update

---
+++ Contents/Items/RelicItem/RelicSetContent/RelicSet_NatureOfGoodAndEvil.cs
@@ -0,0 +1,52 @@
+﻿using Roguelike.Common.Global;
+using Roguelike.Common.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Roguelike.Contents.Items.RelicItem.RelicSetContent;
+public class NatureOfGoodAndEvil_ModPlayer : ModPlayer {
+	class NatureOfGoodAndEvil : RelicSet {
+		public override void SetStaticDefaults() {
+			Requirement = 5;
+		}
+	}
+	public bool set => RelicSetSystem.Check_RelicSetRequirment(Player, RelicSet.GetRelicSetType<NatureOfGoodAndEvil>());
+	public int Stack = 0;
+	public int Decay = 0;
+	public override void ResetEffects() {
+		if (set) {
+			Decay = ModUtils.CountDown(Decay);
+			if (Stack > 0 && Decay <= 0) {
+				Stack--;
+				Decay = 150;
+			}
+		}
+	}
+	public override void UpdateEquips() {
+		if (Stack <= 0 || !set) {
+			return;
+		}
+		PlayerStatsHandle modplayer = Player.GetModPlayer<PlayerStatsHandle>();
+		if (Player.IsHealthAbovePercentage(.5f)) {
+			modplayer.AddStatsToPlayer(PlayerStats.PureDamage, Additive: 1 + Stack * .05f);
+			modplayer.AddStatsToPlayer(PlayerStats.AttackSpeed, Additive: 1 + Stack * .03f);
+		}
+		else {
+			modplayer.AddStatsToPlayer(PlayerStats.RegenHP, Additive: 1 + .1f * Stack, Flat: 2 * Stack);
+		}
+	}
+	public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+		if (!set) {
+			return;
+		}
+		if (hit.Crit) {
+			Stack = Math.Clamp(Stack + 1, 0, 5);
+			Decay = 150;
+		}
+	}
+}
